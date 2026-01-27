dcd89ecdc91eedff76d8a22a6ee34712115c1a7f
JakobKlotz <jakob.klotz@mci.edu>
2026-01-27 15:15:30 +0100

    fix: live reload

--- dev.py
+++ dev.py
@@ -6,5 +6,9 @@ env = os.environ.copy()
 env["ENABLE_GIT_AUTHORS_SERVE"] = "false"
 env["ENABLE_GIT_REVISION_DATE"] = "false"
 
-result = subprocess.run(["uv", "run", "mkdocs", "serve"], env=env)
+# workaround to enable live reload
+# see https://github.com/squidfunk/mkdocs-material/issues/8478
+result = subprocess.run(
+    ["uv", "run", "mkdocs", "serve", "--livereload"], env=env
+)
 sys.exit(result.returncode)
